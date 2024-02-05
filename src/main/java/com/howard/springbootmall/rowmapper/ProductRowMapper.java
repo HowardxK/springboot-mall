c89b0e1bf19623d8bf18bfd3b5f4a1450d4166e3
Howard Chang <howard68790@gmail.com>
2024-02-05 16:46:00 +0800

    商品功能 - Refactor category，使用 Enum 類型

--- src/main/java/com/howard/springbootmall/rowmapper/ProductRowMapper.java
+++ src/main/java/com/howard/springbootmall/rowmapper/ProductRowMapper.java
@@ -1,5 +1,6 @@
 package com.howard.springbootmall.rowmapper;
 
+import com.howard.springbootmall.constant.ProductCategory;
 import com.howard.springbootmall.model.Product;
 import org.springframework.jdbc.core.RowMapper;
 
@@ -11,15 +12,21 @@ public class ProductRowMapper implements RowMapper<Product> {
     public Product mapRow(ResultSet rs, int rowNum) throws SQLException {
         Product product = new Product();
 
-        product.setProduct_id(rs.getInt("product_id"));
-        product.setProduct_name(rs.getString("product_name"));
-        product.setCategory(rs.getString("category"));
-        product.setImage_url(rs.getString("image_url"));
+        product.setProductId(rs.getInt("product_id"));
+        product.setProductName(rs.getString("product_name"));
+
+//        String categoryStr = rs.getString("category");
+//        ProductCategory category = ProductCategory.valueOf(categoryStr);
+//        product.setCategory(category);
+
+        product.setCategory(ProductCategory.valueOf(rs.getString("category")));
+
+        product.setImageUrl(rs.getString("image_url"));
         product.setPrice(rs.getInt("price"));
         product.setStock(rs.getInt("stock"));
         product.setDescription(rs.getString("description"));
-        product.setCreated_date(rs.getTimestamp("created_date"));
-        product.setLast_modified_date(rs.getTimestamp("last_modified_date"));
+        product.setCreatedDate(rs.getTimestamp("created_date"));
+        product.setLastModifiedDate(rs.getTimestamp("last_modified_date"));
 
         return product;
     }
